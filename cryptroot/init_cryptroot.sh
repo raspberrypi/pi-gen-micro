4bcfad61fe146702f70b4621d0ffb4b5a03676cd
Tom Dewey <tom.dewey@raspberrypi.com>
2024-09-03 20:04:48 +0100

    cryptroot: init: Shellcheck fixes

--- cryptroot/init_cryptroot.sh
+++ cryptroot/init_cryptroot.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-/sbin/insmod /lib/modules/$(uname -r)/kernel/drivers/md/dm-mod.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/drivers/md/dm-crypt.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/crypto/af_alg.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/crypto/algif_skcipher.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/lib/crypto/libaes.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/crypto/aes_generic.ko.xz
-/sbin/insmod /lib/modules/$(uname -r)/kernel/arch/arm64/crypto/aes-arm64.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-mod.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-crypt.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/af_alg.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/algif_skcipher.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/lib/crypto/libaes.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/aes_generic.ko.xz
+/sbin/insmod /lib/modules/"$(uname -r)"/kernel/arch/arm64/crypto/aes-arm64.ko.xz
 
 PRIVATE_KEY=$(/usr/local/bin/rpi-otp-private-key -b | base64 -w0)
-echo -n $PRIVATE_KEY > /data/fastboot.key
-printf "${PRIVATE_KEY}\r\n" >> /data/private.key
+printf '%s' "${PRIVATE_KEY}" > /data/fastboot.key
+printf '%s' "${PRIVATE_KEY}\r\n" >> /data/private.key
 
 /usr/bin/cryptkey-fetch | /sbin/cryptsetup luksOpen --key-file=/data/fastboot.key /dev/mmcblk0p2 cryptroot
 
