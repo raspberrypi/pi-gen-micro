0ca7e358a8a5255904882b052de1619724aba172
Tom Dewey <tom.dewey@raspberrypi.com>
2024-10-03 13:42:30 +0100

    cryptroot: Refactor
    1) Remove rpi-otp-key, not required
    2) Remove extraneous --key-file directive, harmful
    3) Remove kernel module explicit loading, not required
    4) Compress with zstd
    5) Move binaries to /usr/bin

--- cryptroot/init_cryptroot.sh
+++ cryptroot/init_cryptroot.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-mod.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-crypt.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/af_alg.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/algif_skcipher.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/lib/crypto/libaes.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/aes_generic.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/arch/arm64/crypto/aes-arm64.ko.xz
+set -e
 
-PRIVATE_KEY=$(/usr/local/bin/rpi-otp-private-key -b | base64 -w0)
-printf '%s' "${PRIVATE_KEY}" > /data/fastboot.key
-printf '%s' "${PRIVATE_KEY}\r\n" >> /data/private.key
+/usr/bin/cryptkey-fetch | /sbin/cryptsetup luksOpen /dev/mmcblk0p2 cryptroot
 
-/usr/bin/cryptkey-fetch | /sbin/cryptsetup luksOpen --key-file=/data/fastboot.key /dev/mmcblk0p2 cryptroot
+/usr/bin/busybox mount /dev/mapper/cryptroot /mnt
 
-/bin/busybox mount /dev/mapper/cryptroot /mnt
-
-/bin/busybox mount /dev/mmcblk0p1 /mnt/boot/firmware
+/usr/bin/busybox mount /dev/mmcblk0p1 /mnt/boot/firmware
 
 systemctl switch-root /mnt /usr/sbin/init
