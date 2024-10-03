698a6b590da8717e9b9e440172c810acd990f135
Richard Oliver <richard.oliver@raspberrypi.com>
2024-10-03 16:39:08 +0100

    Remove python dependencies

--- app/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(
-        name='pi-gen-micro',
-        version='0.1',
-        packages=["configure"],
-        license='MIT',
-        long_description=open('README.txt').read(),
-)
--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-setup(
-        name='pi-gen-micro',
-        version='0.0.1',
-        packages=['app'],
-        license='MIT',
-)
