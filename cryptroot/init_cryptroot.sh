c058ba2799dc2cd0e143e19d9631f7f17bb6729a
Tom Dewey <tom.dewey@raspberrypi.com>
2024-11-22 16:57:13 +0000

    cryptroot: Update to remove manual kernel module insertion

--- cryptroot/init_cryptroot.sh
+++ cryptroot/init_cryptroot.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-mod.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/drivers/md/dm-crypt.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/af_alg.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/algif_skcipher.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/lib/crypto/libaes.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/crypto/aes_generic.ko.xz
-/sbin/insmod /lib/modules/"$(uname -r)"/kernel/arch/arm64/crypto/aes-arm64.ko.xz
-
 /usr/bin/cryptkey-fetch | /sbin/cryptsetup luksOpen /dev/mmcblk0p2 cryptroot
 
 /usr/bin/busybox mount /dev/mapper/cryptroot /mnt
