dcc7461d4688020fb778d34df60ab65a66bf5f20
Ben Benson <benbenson2004@gmail.com>
2025-07-31 10:21:26 +0100

    Added post configuration script Tidied up rpi-embedded-imager profile

---
+++ configurations/rpi-imager-embedded/post_creation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf build/usr/lib/udev/hwdb.d
+rm -rf build/usr/lib/udev/hwdb.bin
+rm -rf build/usr/lib/udev/hwdb.d/20-pci-vendor-model.hwdb
+rm -rf build/usr/lib/udev/hwdb.d/20-OUI.hwdb
+rm -rf build/usr/share/doc
+rm -rf build/usr/lib/udev/hwdb.d/20-usb-vendor-model.hwdb
+rm -rf build/usr/share/libwacom
+rm -rf build/var/lib/dpkg/info
