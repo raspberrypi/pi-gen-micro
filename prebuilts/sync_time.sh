3a094c070cbad8d542dc5fa891dc879cb6a7631f
Ben Benson <benbenson2004@gmail.com>
2025-08-22 15:39:56 +0100

    Added build options for different devices
    Enter the devices list as a comma separated array to tune the image for the specific devices
    e.g. pi-gen-micro rpi-imager-embedded pi5,cm5,500,pi4
    
    Also adds timesync services

--- prebuilts/sync_time.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-TIME_STR=$(wget -q -S -O /dev/null --no-verbose --continue http://downloads.raspberrypi.com 2>&1 | grep -i 'date:' | awk -F': ' '{print $2}')
-if [ -n "$TIME_STR" ]; then
-    date -s "$TIME_STR"
-    echo "Time set to: $(date)"
-else
-    echo "Failed to get time from server."
-fi
